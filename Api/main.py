44edb311ce3cb6c08fa4db95020b8725a0c5bbbe
ASTEP <s.stepantzover@yandex.ru>
2025-05-15 18:00:42 +0300

    feat(common): change username to email and clear CORS

--- Api/main.py
+++ Api/main.py
@@ -2,11 +2,24 @@ from fastapi import FastAPI, WebSocket
 from auth import routes as auth_routes
 from websocket.socket import websocket_endpoint
 from database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+
+]
+
+# Добавление CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],                # разрешённые источники
+    allow_credentials=True,
+    allow_methods=["*"],                  # разрешённые HTTP методы
+    allow_headers=["*"],                  # разрешённые заголовки
+)
 # Роуты
 app.include_router(auth_routes.router)
 
