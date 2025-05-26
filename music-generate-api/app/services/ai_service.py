a359b903f885924d745ef1faaeec7f87c77a531e
Дон <dsvinkabot@gmail.com>
2025-05-26 19:02:17 +0300

    HotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfix
    HotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfixHotfix

--- music-generate-api/app/services/ai_service.py
+++ music-generate-api/app/services/ai_service.py
@@ -27,12 +27,12 @@ class AiService:
 
         print("[AI] Ready!")
 
-    def generate(self, *prompt: str, output_path: str="/out/result.mp3", context_size: int = 1024):
+    def generate(self, prompt: str, output_path: str="out/result.mp3", context_size: int = 1024):
         self.is_busy = True
 
         print("[AI] Setup Generation...")
         inputs = self.__processor(
-            text=list(prompt),
+            text=[prompt],
             padding=True,
             return_tensors="pt",
         )
