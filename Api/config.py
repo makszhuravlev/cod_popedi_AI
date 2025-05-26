bcd81b80c21c4b42feaf62894bb7041802492c73
Wladimir17 <101710090+Wladimir17@users.noreply.github.com>
2025-05-27 01:23:14 +0300

    Исправление ошибок, связанных с упрощением

--- Api/config.py
+++ Api/config.py
@@ -1,11 +1,13 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:9090@localhost/cod_popedi_AI")
-SECRET_KEY = os.getenv("SECRET_KEY", "secret123")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 IP_AI = "10.0.8.15"
 IP_TEXT_AI = "10.0.8.15:11434"
 IP_BACK = "10.0.8.15"
 PORT_BACK = "8000"
+
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://Adept0mSimerol1S:AveImperium!@"+IP_BACK+"/cod_pobedi_ai")
+SECRET_KEY = os.getenv("SECRET_KEY", "secret123")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
--- frontik/vite.config.js
+++ frontik/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ["katuscha.ssrv.su", "localhost", "127.0.0.1", "10.0.8.15"],
+  },
+});
