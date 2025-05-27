c8787f67e93d3cec1cbaa3e4c3c7c5b778897e2d
Wladimir17 <101710090+Wladimir17@users.noreply.github.com>
2025-05-28 00:34:13 +0300

    Обновляем данные, теперь должно работать

--- Api/config.py
+++ Api/config.py
@@ -1,6 +1,6 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://Adept0mSimerol1S:AveImperium!@10.0.8.15/cod_pobedi_ai")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://adept0msimerol1s:AveImperium!@10.0.8.15/cod_popedi_ai")
 SECRET_KEY = os.getenv("SECRET_KEY", "secret123")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
