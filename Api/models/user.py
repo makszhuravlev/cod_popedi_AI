9bf84eb91b2e460ec6f385e2c71146281d701124
Astep <s.stepantzover@yandex.ru>
2025-05-27 23:54:37 +0300

    Fix long еуче

--- Api/models/user.py
+++ Api/models/user.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Enum, Index
-from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -30,7 +29,7 @@ class GeneratedFile(Base):
     __tablename__ = "generated_files"
     id = Column(Integer, primary_key=True, index=True)
     request_id = Column(Integer, ForeignKey("requests.id"))
-    file_url = Column(LONGTEXT, nullable=False)
+    file_url = Column(String(1000), nullable=False)
     file_type = Column(Enum('image', 'music', 'text', 'gift', name="file_type_enum"), nullable=False)
     
     request = relationship("Request", back_populates="files")
